425ee49050f8edd45867ec7eae0a5ac6adc7f4d5
Daniel Hanikka <daniel.hanikka@proton.me>
2025-12-11 05:08:54 +0200

    added my_aliases.sh

---
+++ aliases/my_aliases.sh
@@ -0,0 +1,37 @@
+# misc
+alias lsaa="lsd -al"
+alias fafe="clear && fastfetch"
+
+# flatpak
+alias fl-in="flatpak install"
+alias fl-rm="flatpak uninstall"
+alias fl-up="flatpak update"
+alias fl-r="flatpak run"
+
+# apt
+alias ap-in="sudo apt install"
+alias ap-up="sudo apt update && sudo apt upgrade"
+alias ap-s="apt search"
+alias ap-i="apt show"
+
+# dnf
+alias dn-up="sudo dnf upgrade"
+alias dn-in="sudo dnf install"
+alias dn-rm="sudo dnf remove"
+alias dn-s="dnf search"
+alias dn-i="dnf info"
+
+# pacman
+alias p-up="sudo pacman -Syu"
+alias p-in="sudo pacman -S "
+alias p-rm="sudo pacman -R "
+alias p-s="pacman -Ss "
+alias p-i="pacman -Si "
+
+# nala
+# alias n-in="sudo nala install "
+# alias n-rm="sudo nala remove "
+# alias n-up="sudo nala update && sudo nala upgrade"
+# alias n-s="nala search "
+# alias n-i="nala show "
+
