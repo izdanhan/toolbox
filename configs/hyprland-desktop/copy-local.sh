ee935698e694c38cf2bd8d2afe1d40e1ab6fe9a7
Daniel Hanikka <daniel.proton.me>
2026-08-23 20:34:17 +0300

    ...

--- configs/hyprland-desktop/copy-local.sh
+++ configs/hyprland-desktop/copy-local.sh
@@ -7,8 +7,10 @@ cp -rv ~/.config/fresh .
 cp -rv ~/.config/ghostty .
 cp -rv ~/.config/hypr .
 cp -rv ~/.config/kitty .
+cp -rv ~/.config/MangoHud .
 cp -rv ~/.config/mpv .
 cp -rv ~/.config/rofi .
 cp -rv ~/.config/superfile .
 cp -rv ~/.config/tilix .
 cp -rv ~/.config/waybar .
+cp -rv ~/.zshrc .
